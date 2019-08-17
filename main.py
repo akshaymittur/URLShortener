65220c67e723ac83a3704b50529d4b1274b297fb
Akshay Mittur <akshaymittur@gmail.com>
2019-08-17 23:58:24 +0530

    Changed Encoding Algo

--- main.py
+++ main.py
@@ -13,48 +13,43 @@ app = Flask(__name__)
 host = 'http://localhost:5000/'
 
 class UrlShortener:
-    id = 1
-    urltoid = {}
+    characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    base = len(characters)
 
-    def shortenUrl(self, originalUrl):
-        if original_url in self.urltoid:
-            id = self.urltoid[originalUrl]
-            shortenUrl = self.encode(id)
+    def shortenUrl(self, id):
+        
+        val = id % base
+        res = characters[val]
+        index = id // base
 
-        else:
-            self.urltoid[originalUrl] = self.id
-            shortenUrl = self.encode(self.id)
-            self.id += 1
+        while index:
+            val = index % base
+            index = id // base
+            res = characters[int(val)] + res
 
-        return host + str(shortenUrl)
-    
-    def encode(self, id):
-        characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        base = len(characters)
+        return res
 
-        short = []
+    def decode(self, encodedurl):
+        limit = len(encodedurl)
+        res = 0
+        for i in range(limit):
+            res = base * res + characters.find(encodedurl[i])
+        
+        return res
 
-        while id > 0:
-            val = id % base
-            short.append(characters[val])
-            id = id // base
-
-        return "".join(short[::-1])
-
- 
     def check_table(self):
-    create_table = """
-        CREATE TABLE WEBSITE_URLS(
-            ID INT PRIMARY KEY AUTOINCREMENT,
-            URL TEXT NOT NULL
-        );
-        """
-    with sqlite3.connect('urls.db') as conn:
-        cursor = conn.cursor()
-        try:
-            cursor.execute(create_table)
-        except OperationalError:
-            pass
+        create_table = """
+            CREATE TABLE WEBSITE_URLS(
+                ID INT PRIMARY KEY AUTOINCREMENT,
+                URL TEXT NOT NULL
+            );
+            """
+        with sqlite3.connect('urls.db') as conn:
+            cursor = conn.cursor()
+            try:
+                cursor.execute(create_table)
+            except OperationalError:
+                pass
 
 Shortener = UrlShortener()
 
@@ -67,36 +62,17 @@ def home():
         else:
             url = originalUrl
 
-        with sqlite3.connect('urls.db') ass conn:
+        with sqlite3.connect('urls.db') as conn:
             cursor = conn.cursor
             res = cursor.execute(
                 'INSERT INTO WEB_URL (URL) VALUES (?)',
                 [base64.urlsafe_b64encode(url)]
                 )
-            encoded_string = Shortener.shortenUrl(url)
+            encoded_string = Shortener.shortenUrl(res.lastrowid)
 
-        return render_template('home.html', short_url = encoded_string)
+        return render_template('home.html', short_url = host + encoded_string)
 
     return render_template('home.html')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
